cd4f4494e3e98eddf268d143f8f6a753d664aa59
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-07-28 14:03:45 +0530

    change logic and add alerts

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
-function genarateId (){
-    let genaratedNum = (Math.random()*10)+2;
-    let txtvalue = parseInt(document.getElementById('txtbox').value);
-    let roundnum = Math.round(genaratedNum);
-    console.log(roundnum);
+//genarated random number between 2 to 12
+let randomnum;
+let count = 3;
+function genarateRandomeNumber(){
+    let genaratedNum;
+    genaratedNum = Math.random() * 10 + 2;
+     randomnum = Math.round(genaratedNum);
+    console.log(genaratedNum);
+    console.log(randomnum);
+    if(2 < randomnum & randomnum < 7){
+        console.log("genarated number grater than 2 and lessthan 7");
+    }else if(7<randomnum & randomnum<12){
+        console.log("genarated number grater than 7 and lessthan 12");
+    }
+}
+genarateRandomeNumber();
+//genarate id function
+function genarateId() {
+    let txtvalue = parseInt(document.getElementById("txtbox").value);
     console.log(txtvalue);
 
-    // logic
-if (roundnum==txtvalue) {
-    console.log("YOU WIN");
-}
-else if (roundnum>2 && 5<roundnum) {
-    console.log("Num is grater than 2 and lessthn 5 and Try again...");
-    // console.log("Try again...");
-} else if(roundnum>5 && 10<roundnum){
-    console.log("Num is grater than 5 and lessthn 10 and Try again...");
-    // console.log("Try again...");
-}else{
-    console.log("Num is grater 10 and Try again...");
-    // console.log("Try again...");
-}
+    if(isNaN(txtvalue) || txtvalue<2 || txtvalue>12){
+        Swal.fire({
+            title: "Invalid Number!",
+            text: "Pleas enter a number between 2 to 12",
+            icon: "warning"
+          });
+    }
+    if(randomnum == txtvalue){
+        Swal.fire({
+            title: "congratulations!",
+            text: "You Win The Game",
+            icon: "success"
+          });
+          genarateRandomeNumber();
+    }else{
+        count--;
+        if(count > 0){
+            Swal.fire({
+                title: "Try Again!",
+                text: `You have ${count} attemts left.`,
+                icon: "info"
+              });
+        }else{
+            Swal.fire({
+                title: "Try Again!",
+                text: `You lost game`,
+                icon: "info"
+              });
+            genarateRandomeNumber();
+        }
+    }
 
 }
 
-// logic
