d2413f0510d8742a8817e4e6bfcbea87643d1875
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-07-28 19:51:51 +0530

    add some styles

--- app.js
+++ app.js
@@ -1,55 +1,56 @@
 //genarated random number between 2 to 12
 let randomnum;
 let count = 3;
-function genarateRandomeNumber(){
-    let genaratedNum;
-    genaratedNum = Math.random() * 10 + 2;
-     randomnum = Math.round(genaratedNum);
-    console.log(genaratedNum);
-    console.log(randomnum);
-    if(2 < randomnum & randomnum < 7){
-        console.log("genarated number grater than 2 and lessthan 7");
-    }else if(7<randomnum & randomnum<12){
-        console.log("genarated number grater than 7 and lessthan 12");
-    }
+function genarateRandomeNumber() {
+  let genaratedNum;
+  genaratedNum = Math.random() * 10 + 2;
+  randomnum = Math.round(genaratedNum);
+  console.log(genaratedNum);
+  console.log(randomnum);
+  if ((2 < randomnum) & (randomnum < 7)) {
+    document.getElementById("hint").innerHTML =
+      "hint : Gnarated number is greater than 2 and less than 7";
+  } else if ((7 < randomnum) & (randomnum < 12)) {
+    document.getElementById("hint").innerHTML =
+      "hint : Genarated number Greater than 7 and less than 12";
+  }
 }
 genarateRandomeNumber();
 //genarate id function
 function genarateId() {
-    let txtvalue = parseInt(document.getElementById("txtbox").value);
-    console.log(txtvalue);
+  let txtvalue = parseInt(document.getElementById("txtbox").value);
+  console.log(txtvalue);
 
-    if(isNaN(txtvalue) || txtvalue<2 || txtvalue>12){
-        Swal.fire({
-            title: "Invalid Number!",
-            text: "Pleas enter a number between 2 to 12",
-            icon: "warning"
-          });
+  if (isNaN(txtvalue) || txtvalue < 2 || txtvalue > 12) {
+    Swal.fire({
+      title: "Invalid Number!",
+      text: "Pleas enter a number between 2 to 12",
+      icon: "warning",
+    });
+    return;
+  }
+  if (randomnum == txtvalue) {
+    Swal.fire({
+      title: "congratulations!",
+      text: "You Win The Game",
+      icon: "success",
+    });
+    genarateRandomeNumber();
+  } else {
+    count--;
+    if (count > 0) {
+      Swal.fire({
+        title: "Try Again!",
+        text: `You have ${count} attemts left.`,
+        icon: "info",
+      });
+    } else {
+      Swal.fire({
+        title: "Try Again!",
+        text: `You lost game`,
+        icon: "warning",
+      });
+      genarateRandomeNumber();
     }
-    if(randomnum == txtvalue){
-        Swal.fire({
-            title: "congratulations!",
-            text: "You Win The Game",
-            icon: "success"
-          });
-          genarateRandomeNumber();
-    }else{
-        count--;
-        if(count > 0){
-            Swal.fire({
-                title: "Try Again!",
-                text: `You have ${count} attemts left.`,
-                icon: "info"
-              });
-        }else{
-            Swal.fire({
-                title: "Try Again!",
-                text: `You lost game`,
-                icon: "info"
-              });
-            genarateRandomeNumber();
-        }
-    }
-
+  }
 }
-
